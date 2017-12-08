a5962611a7138e1bdafe978910426830cced5a71
Pulkit Gupta <pulkonet@gmail.com>
2017-12-08 22:48:23 +0530

    bottom nav added, with fragments

---
+++ app/src/main/java/com/cognizance/cognizance18/WorkshopsFragment.java
@@ -0,0 +1,27 @@
+package com.cognizance.cognizance18;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkshopsFragment extends Fragment {
+    public static WorkshopsFragment newInstance() {
+        WorkshopsFragment fragment = new WorkshopsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workshops, container, false);
+    }
+}
