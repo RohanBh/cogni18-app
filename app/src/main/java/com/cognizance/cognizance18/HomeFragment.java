6e4faa3491331a379e43baa42685a61ccc5f9367
Pulkit Gupta <pulkonet@gmail.com>
2017-12-09 01:21:50 +0530

    static cards added to homefragment

--- app/src/main/java/com/cognizance/cognizance18/HomeFragment.java
+++ app/src/main/java/com/cognizance/cognizance18/HomeFragment.java
@@ -1,7 +1,5 @@
 package com.cognizance.cognizance18;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,11 +16,13 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        return rootView;
     }
 }
