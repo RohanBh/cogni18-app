70932d0af911ecf320836f961b49939ded5771c4
Arihant Jain <aridj456@gmail.com>
2017-12-12 16:19:21 +0530

    carousel added in home fragement

--- app/src/main/java/com/cognizance/cognizance18/HomeFragment.java
+++ app/src/main/java/com/cognizance/cognizance18/HomeFragment.java
@@ -2,12 +2,22 @@ package com.cognizance.cognizance18;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cognizance.cognizance18.adapters.EventsRViewAdapter;
+import com.cognizance.cognizance18.models.Event;
+
+import java.util.ArrayList;
+
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView spotLightsRecyclerView, eventsRecyclerView, workshopsRecyclerView;
+
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
         return fragment;
@@ -22,7 +32,40 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        return rootView;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initViews(view);
+        setupViews();
+        initListeners();
+        return view;
+    }
+    private void initViews(View view) {
+        spotLightsRecyclerView = view.findViewById(R.id.spot_light_recycler_view);
+        eventsRecyclerView = view.findViewById(R.id.events_recycler_view);
+        workshopsRecyclerView = view.findViewById(R.id.workshops_recycler_view);
+    }
+
+    private void setupViews() {
+        ArrayList<Event> eventsList;
+        eventsList = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            Event event = new Event();
+            event.setTitle("Event" + (i+1));
+            eventsList.add(event);
+        }
+        spotLightsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()
+                ,LinearLayoutManager.HORIZONTAL,false));
+        spotLightsRecyclerView.setAdapter(new EventsRViewAdapter(getContext(),eventsList));
+
+        eventsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()
+                ,LinearLayoutManager.HORIZONTAL,false));
+        eventsRecyclerView.setAdapter(new EventsRViewAdapter(getContext(),eventsList));
+
+        workshopsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()
+                ,LinearLayoutManager.HORIZONTAL,false));
+        workshopsRecyclerView.setAdapter(new EventsRViewAdapter(getContext(),eventsList));
+    }
+
+    private void initListeners() {
+
     }
 }
---
+++ app/src/main/java/com/cognizance/cognizance18/models/Event.java
@@ -0,0 +1,17 @@
+package com.cognizance.cognizance18.models;
+
+/**
+ * Created by arihant on 12/12/17.
+ */
+
+public class Event {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
