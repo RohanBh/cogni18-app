4e6a65177dcb00208135922b345c8287716b4d3b
RohanBh <brohan52@gmail.com>
2017-12-23 14:51:19 +0530

    Change WorkshopsFragment.java to WorkshopsFragment.kt

---
+++ app/src/main/java/com/cognizance/cognizance18/WorkshopsFragment.kt
@@ -0,0 +1,27 @@
+package com.cognizance.cognizance18
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WorkshopsFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): WorkshopsFragment {
+            return WorkshopsFragment()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_workshops, container, false)
+    }
+
+}
