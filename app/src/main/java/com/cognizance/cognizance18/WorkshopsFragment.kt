653f27e18a551c08256603c1a6ca179f155c6317
RohanBh <brohan52@gmail.com>
2017-12-23 19:13:12 +0530

    Complete the workshop fragment

--- app/src/main/java/com/cognizance/cognizance18/WorkshopsFragment.kt
+++ app/src/main/java/com/cognizance/cognizance18/WorkshopsFragment.kt
@@ -2,11 +2,19 @@ package com.cognizance.cognizance18
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cognizance.cognizance18.adapters.WorkshopsRViewAdapter
+import com.cognizance.cognizance18.models.Event
+import kotlinx.android.synthetic.main.fragment_workshops.*
 
 class WorkshopsFragment : Fragment() {
+
+    private val mEventList by lazy {
+        generateFakeData()
+    }
 
     companion object {
         @JvmStatic
@@ -19,9 +27,21 @@ class WorkshopsFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.fragment_workshops, container, false)
+    override fun onCreateView(
+            inflater: LayoutInflater?,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ) = inflater?.inflate(R.layout.fragment_workshops, container, false)
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        workshops_recycler_view.adapter = WorkshopsRViewAdapter(mEventList)
+        workshops_recycler_view.layoutManager = LinearLayoutManager(context)
     }
+
+    private fun generateFakeData() = List<Event>(10) {
+        Event("Fake Event ${it + 1}", getString(R.string.placeholder_text), "24-25 March 2018")
+    }
+
 
 }
