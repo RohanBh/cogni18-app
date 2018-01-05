f64c68ed7c2b6bebc0ab6d989134a14db31a92d9
Arihant Jain <aridj456@gmail.com>
2018-01-05 14:33:11 +0530

    Nav Drawer with basic design added

--- app/src/main/java/com/cognizance/cognizance18/MainActivity.java
+++ app/src/main/java/com/cognizance/cognizance18/MainActivity.java
@@ -3,19 +3,42 @@ package com.cognizance.cognizance18;
 import android.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.front_activity);
+        setContentView(R.layout.activity_main);
+
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Navigation View
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        // Bottom Navigation View
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         bottomNavigationView.getMenu().getItem(2).setChecked(true);
         bottomNavigationView.setOnNavigationItemSelectedListener
@@ -50,6 +73,39 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, HomeFragment.newInstance());
         transaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        android.support.v4.app.Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                selectedFragment = HomeFragment.newInstance();
+                break;
+            case R.id.nav_events:
+                selectedFragment = EventsFragment.newInstance();
+                break;
+            case R.id.nav_profile:
+                selectedFragment = ProfileFragment.newInstance();
+                break;
+            case R.id.nav_workshops:
+                selectedFragment = WorkshopsFragment.newInstance();
+                break;
+            case R.id.nav_spotlights:
+                selectedFragment = SpotlightFragment.newInstance();
+                break;
+            case R.id.nav_sign_out:
+                Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, selectedFragment);
+        transaction.commit();
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
 
