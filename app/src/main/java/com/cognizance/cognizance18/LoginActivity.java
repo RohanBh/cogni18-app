83b5d3f72d72b6c11160aa21bdc89b96ca029bcb
RohanBh <brohan52@gmail.com>
2018-01-08 01:05:59 +0530

    Add code for google sign in

--- app/src/main/java/com/cognizance/cognizance18/LoginActivity.java
+++ app/src/main/java/com/cognizance/cognizance18/LoginActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.cognizance.cognizance18.models.LoginResponse;
 import com.cognizance.cognizance18.models.OauthUser;
 import com.cognizance.cognizance18.models.User;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,12 +27,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 219;
+
     private TextInputEditText emailEditText;
     private TextInputEditText phoneEditText;
     private AppCompatButton getStartedButton;
     private final String BASE_URL = "https://api.cognizance.org.in/";
     private final String LOG_TAG = "LoginActivity :";
     private SessionManager session;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +48,70 @@ public class LoginActivity extends AppCompatActivity {
         //initialising login views
         initViews();
 
+        initVariables();
+
         // set click listeners
         setClickListeners();
     }
 
-    private void initViews(){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void initVariables() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(LOG_TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
+            // No account exists
+        } else {
+            // user is signed in
+        }
+    }
+
+    private void initViews() {
         emailEditText = findViewById(R.id.email_edit_text);
         phoneEditText = findViewById(R.id.mobile_number_edit_text);
         getStartedButton = findViewById(R.id.get_started_btn);
 
     }
 
-    private void setClickListeners(){
+    private void setClickListeners() {
         getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,16 +120,27 @@ public class LoginActivity extends AppCompatActivity {
                 verifyFromAPI(email, password);
             }
         });
+        findViewById(R.id.sign_in_button).setOnClickListener(
+                (View v) -> {
+                    switch (v.getId()) {
+                        case R.id.sign_in_button: {
+                            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                            startActivityForResult(signInIntent, RC_SIGN_IN);
+                            break;
+                        }
+                    }
+                }
+        );
     }
 
-    private void verifyFromAPI(String email, String password){
-        User user = new User(email,password);
+    private void verifyFromAPI(String email, String password) {
+        User user = new User(email, password);
         ApiInterface apiInterface = this.getInterfaceService();
         Call<LoginResponse> service = apiInterface.authenticate(user);
         service.enqueue(new Callback<LoginResponse>() {
             @Override
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                if (response.code() == 200){
+                if (response.isSuccessful()){
                     session.createLoginSession(response.body());
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
@@ -76,7 +149,6 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 else
                     Toast.makeText(LoginActivity.this, "Error : " + response.code(), Toast.LENGTH_SHORT).show();
-
             }
 
             @Override
@@ -95,7 +167,7 @@ public class LoginActivity extends AppCompatActivity {
         service.enqueue(new Callback<LoginResponse>() {
             @Override
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                if (response.code() == 200) {
+                if (response.isSuccessful()) {
                     session.createLoginSession(response.body());
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
