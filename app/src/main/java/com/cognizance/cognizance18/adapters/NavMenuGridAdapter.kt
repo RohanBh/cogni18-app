9c2f963d097df58040ee26673a15f50dc4c954dc
RohanBh <brohan52@gmail.com>
2018-01-19 11:23:57 +0530

    Complete the more section

--- app/src/main/java/com/cognizance/cognizance18/adapters/NavMenuGridAdapter.kt
+++ app/src/main/java/com/cognizance/cognizance18/adapters/NavMenuGridAdapter.kt
@@ -1,10 +1,10 @@
 package com.cognizance.cognizance18.adapters
 
 import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
 import android.widget.BaseAdapter
 import com.cognizance.cognizance18.R
 import com.cognizance.cognizance18.models.NavMenuGridItem
@@ -14,17 +14,19 @@ import kotlinx.android.synthetic.main.item_nav_menu.view.*
 /**
  * Created by rohan on 18/1/18.
  */
-class NavGridMenuAdapter(val menuItems: List<NavMenuGridItem>, val rowHeight: Int) : BaseAdapter() {
+class NavMenuGridAdapter(val menuItems: List<NavMenuGridItem>, val rowHeight: Int = 0) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val row = LayoutInflater.from(parent.context).inflate(R.layout.item_nav_menu, parent, false)
-        val lp = AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, rowHeight)
-        row.layoutParams = lp
+        /*val lp = AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, rowHeight)
+        row.layoutParams = lp*/
 
+        Log.d("$this", "pos : $position, text : ${menuItems[position].text}")
         row.item_text.text = menuItems[position].text
 
-        row.ic_item.setImageDrawable(ContextCompat.getDrawable(parent.context,
-                menuItems[position].imageResource))
+        menuItems[position].imageResource?.let {
+            row.ic_item.setImageDrawable(ContextCompat.getDrawable(parent.context, it))
+        }
         return row
     }
 
--- app/src/main/java/com/cognizance/cognizance18/models/MiscModels.kt
+++ app/src/main/java/com/cognizance/cognizance18/models/MiscModels.kt
@@ -3,4 +3,4 @@ package com.cognizance.cognizance18.models
 /**
  * Created by rohan on 18/1/18.
  */
-data class NavMenuGridItem(val imageResource: Int, val text: String)
+data class NavMenuGridItem(val text: String, val imageResource: Int? = null)
