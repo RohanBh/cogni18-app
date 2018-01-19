bd56c94e7c5e4b394645a8fb98fe084cec645117
Arihant Jain <aridj456@gmail.com>
2018-01-19 12:24:07 +0530

    change in carousels and minor ui changes

--- app/src/main/java/com/cognizance/cognizance18/MainActivity.java
+++ app/src/main/java/com/cognizance/cognizance18/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements OnFragmentAddedLi
 
     SessionManager session;
 
+    // logic variables
     private int prevNonDialogMenuItemId = -1;
     private boolean isFirstTransaction = true;
 
